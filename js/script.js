0fd47d585ba3bb124f37ad692ed62877df7d8f70
tmkelly28 <tmkelly28@gmail.com>
2015-08-12 14:38:18 -0400

    JLint standards, changed name

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-var button = document.getElementsByTagName("button")[0];
-var taskList = document.getElementById("task-list");
-var taskCount = 0;
+var button = document.getElementsByTagName("button")[0],
+    taskList = document.getElementById("task-list"),
+    taskCount = 0;
 
 function escapeHTML(entry) {
-    return entry.replace(/\<|\>/g, "");
+    "use strict";
+    return entry.replace(/<|>/g, "");
 }
 
 function completeTask() {
+    "use strict";
   //to-do
 }
 
-button.addEventListener("click", function() {
-    var newTaskContent = escapeHTML(document.getElementById("newTask").value);
-  newTaskContent = document.createTextNode(newTaskContent)
-  var newTask = document.createElement("div");
-  newTask.appendChild(newTaskContent);
+button.addEventListener("click", function () {
+    "use strict";
+    var newTaskContent = escapeHTML(document.getElementById("newTask").value),
+        newTask = document.createElement("div");
+    newTaskContent = document.createTextNode(newTaskContent);
+    newTask.appendChild(newTaskContent);
   
-  taskList.appendChild(newTask);
-  taskCount += 1;
-  taskList.lastChild.id="task" + taskCount.toString();
-  taskList.lastChild.className="task";
-  taskList.lastChild.addEventListener("click", completeTask);
+    taskList.appendChild(newTask);
+    taskCount += 1;
+    taskList.lastChild.id = "task" + taskCount.toString();
+    taskList.lastChild.className = "task";
+    taskList.lastChild.addEventListener("click", completeTask);
 });
