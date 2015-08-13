32198bcbb0a5abddab4f9bdf75ffd1e22c40bbd8
tmkelly28 <tmkelly28@gmail.com>
2015-08-13 16:20:24 -0400

    added trash button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-//initialize DOM-related and global variables
+// initialize DOM-related and global variables
 var addNewTaskButton = document.getElementById("addNewTask"),
     doneMenuButton = document.getElementById("doneMenu"),
     taskMenuButton = document.getElementById("taskMenu"),
     allDoneButton = document.getElementById("all-done"),
     allUndoneButton = document.getElementById("all-undone"),
+    trashAllButton = document.getElementById("trash-accept"),
     taskList = document.getElementById("task-list"),
     doneList = document.getElementById("done-task-list"),
     whereAmI = "tasks", //using whereAmI is not a good solution. Todo: replace it
@@ -19,7 +20,7 @@ function toggleTask(event) {
     "use strict";
     if (whereAmI === "tasks") {
         for (var i = 0; i < taskPage.tasks.length; i++) {
-            if (eval(event.srcElement.id) === taskPage.tasks[i].id) {
+            if (Number(event.srcElement.id) === taskPage.tasks[i].id) {
             taskPage.tasks[i].currentPage = doneList;
             donePage.tasks.push(taskPage.tasks[i]);
             taskPage.tasks.splice(i, 1);
@@ -28,7 +29,7 @@ function toggleTask(event) {
         }
     } else if (whereAmI === "done") {
         for (var i = 0; i < donePage.tasks.length; i++) {
-            if (eval(event.srcElement.id) === donePage.tasks[i].id) {
+            if (Number(event.srcElement.id) === donePage.tasks[i].id) {
             donePage.tasks[i].currentPage = taskList;
             taskPage.tasks.push(donePage.tasks[i]);
             donePage.tasks.splice(i, 1);
@@ -122,6 +123,12 @@ allDoneButton.addEventListener("click", toggleAll);
 
 // Mark All Undone button
 allUndoneButton.addEventListener("click", toggleAll);
+
+// Trash All button
+trashAllButton.addEventListener("click", function () {
+    donePage.tasks = [];
+    donePage.drawTasks(donePage.tasks);
+});
 
 // Initialize page objects
 var taskPage = new Page(taskList);
